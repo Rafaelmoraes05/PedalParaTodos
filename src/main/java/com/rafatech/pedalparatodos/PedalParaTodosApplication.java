d429533a93d76b1b7939721dae87ad6f49b7d3d1
Rafael Moraes <rafaelmoraes005@gmail.com>
2025-10-29 21:09:34 -0300

    [first commit]

---
+++ src/main/java/com/rafatech/pedalparatodos/PedalParaTodosApplication.java
@@ -0,0 +1,13 @@
+package com.rafatech.pedalparatodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedalParaTodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedalParaTodosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafatech/pedalparatodos/PedalParaTodosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafatech.pedalparatodos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedalParaTodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
